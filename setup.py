1a1e9115e354225a263100e0a306d24178098cc6
Eu-Bin KIM <tlrndk123@gmail.com>
2021-01-20 21:03:26 +0900

    version 0.0.11: now can handle alternative forms.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='merge_idioms',
-    version='0.0.10',
+    version='0.0.11',
     description='spacy pipeline component for merging idioms',
     url='https://github.com/eubinecto/merge-idioms',
     author='Eu-Bin KIM',
